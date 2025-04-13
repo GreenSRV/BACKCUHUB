256da86a80f022376b9dfa987d755ed7a84e4683
GreenSRV <goodgreengreen111@gmail.com>
2025-04-13 17:11:35 +0700

    localhost change to Port5000 fromhost="http://127.0.0.1:5000" - Green

--- app.py
+++ app.py
@@ -16,4 +16,5 @@ app.mongo = mongo  # Attach Mongo to app for reuse
 app.register_blueprint(user_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="http://127.0.0.1:5000", port=5000, debug=True)
+    
